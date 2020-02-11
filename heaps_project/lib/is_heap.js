e87bbfa134e5d2eb361e457e9790fb18fe9f790d
“Eugene <eugenechung1209@gmail.com>
2020-02-11 18:08:56 -0500

    finish tries

---
+++ heaps_project/lib/is_heap.js
@@ -0,0 +1,17 @@
+// you may assume that the array will always have a null element at the 0-th index
+function isMaxHeap(array, idx=1) {
+    if (array[idx] === undefined) return true;
+    let leftIdx = idx * 2;
+    let rightIdx = idx * 2 + 1;
+    let leftVal = array[leftIdx] === undefined ? -Infinity : array[leftIdx];
+    let rightVal = array[rightIdx] === undefined ? -Infinity: array[rightIdx];
+
+    return array[idx] > leftVal && array[idx] > rightVal 
+        && isMaxHeap(array, leftIdx) 
+        && isMaxHeap(array, rightIdx);
+}
+
+
+module.exports = {
+    isMaxHeap
+};
